a9f764b7845a5b82eb49fce4e01a6fb51b0ad391
yava <yavaonly@gmail.com>
2017-12-27 22:02:13 +0200

    111
    222

---
+++ toc_db.js
@@ -0,0 +1,59 @@
+toc_db = [
+    {
+        "id": 0,
+        "pid":-1,
+        "name": "Категорії"
+    },
+    {
+        "id": 1,
+        "pid":0,
+        "name": "Карти",
+        "url": "http://demo.isgeo.kiev.ua/manager",
+        "title": "Карти",
+        "icon":'img/ico/ico_01.png',
+        "iconOpen": 'img/ico/ico_02.png',
+        "open": true
+    },
+    {
+        "id": 2,
+        "pid":1,
+        "name": "Зайнятість населення",
+        "url": "contents/maps/ELNAU_2007/4102/index.html",
+        "title": "Для відкриття 'Зайнятість населення' - перейдіть за посиланням",
+        "icon":'img/globe.gif'
+    },
+    {
+        "id": 3,
+        "pid":1,
+        "name": "Густота сільських поселень (області)",
+        "url": "contents/maps/ELNAU_2007/4035/index.html",
+        "title": "Для відкриття 'Зайнятість населення' - перейдіть за посиланням",
+        "icon":'img/globe.gif'
+    },
+
+    {
+        "id": 4,
+        "pid":0,
+        "name": "RADATLAS2014",
+        "title": "Карти RADATLAS2014",
+        "icon":'img/ico/ico_01.png',
+        "iconOpen": 'img/ico/ico_02.png',
+        "open": true
+    },
+    {
+        "id": 5,
+        "pid":4,
+        "name": "CONTAMINATION DENSITY BY PLUTONIUM ISOTOPES",
+        "url": "contents/RadAtlas2014/Maps/24088287_091098_20/ref_24088287_091098_20.htm",
+        "title": "CONTAMINATION DENSITY BY PLUTONIUM ISOTOPES",
+        "icon":'img/ico/ico_04.png'
+    },
+    {
+        "id": 6,
+        "pid":4,
+        "name": "PREDICTION OF CONTAMINATION OF THE TERRITORY OF UKRAINE BY AMERICIUM-241 FOR 2056",
+        "url": "contents/RadAtlas2014/Maps/24088287_091098_22/ref_24088287_091098_22.htm",
+        "title": "PREDICTION OF CONTAMINATION OF THE TERRITORY OF UKRAINE BY AMERICIUM-241 FOR 2056",
+        "icon":'img/ico/ico_04.png'
+    }
+];
